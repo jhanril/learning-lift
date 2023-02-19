71170fde24facf806fd4e287dea2653fa5735f9f
Jhanril <jhanrilbucod@gmail.com>
2023-02-20 06:51:46 +0800

    change typo in message

--- src/Pages/pageNotAvailable.jsx
+++ src/Pages/pageNotAvailable.jsx
@@ -10,8 +10,8 @@ function pageNotAvailable() {
           <img src={`${Nothing}`} alt="" />
           <h4 className="mt-5 mb-3 pt-5">Sorry for inconvenience</h4>
           <h5>
-            We appreciate your desired to explore but this page is not availble
-            yet
+            We appreciate your desired to explore but this feature is not
+            available yet. Please come back later.
           </h5>
         </div>
       </div>
